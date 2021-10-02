4a1f63efb2827ab0d85cea7a6a8f7399780b6f93
Данил Бодров <Bodrow19999@mail.ru>
2021-10-02 11:40:41 +0400

    4.1. Аргументы [#295521]

---
+++ src/main/java/ru/job4j/ArgMethod.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+public class ArgMethod {
+    public static void main(String[] args) {
+
+        String name = "Job4j";
+        int age = 33;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+    }
+
+    public static void hello(String name, int age) {
+
+        System.out.println("Hello, " + name);
+    }
+}
