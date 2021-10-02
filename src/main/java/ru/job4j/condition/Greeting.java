e3abec2f3d6acf0252c91606571856e00afbf3f4
Данил Бодров <Bodrow19999@mail.ru>
2021-10-02 13:46:10 +0400

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+
+        String idea = "I like Java! ";
+        int year = 2021;
+        System.out.println(idea);
+        String idea1 = idea + "But I am newbie.";
+        System.out.println(idea1);
+        String idea2 = idea + year;
+        System.out.println(idea2);
+    }
+}
