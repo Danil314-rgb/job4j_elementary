4bd2e5f87605a9a8ba50b472441d4ed122d75103
Данил Бодров <Bodrow19999@mail.ru>
2021-10-04 13:11:27 +0400

    4.2. Заголовок в файле java   [#159036]

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,19 +1,22 @@
 package ru.job4j;
+import static ru.job4j.math.MathFunction.*;
 
 public class Calculator {
     public static void main(String[] args) {
-
-        int two = 2;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        System.out.println("Результат расчёта sumMultiply равен: " + sumMultiply(20, 20));
+        System.out.println("Результат расчёта sumMinusDivision равен: " + sumMinusDivision(20, 10));
+        System.out.println("Результат расчёта sumAllOperation равен: " + sumAllOperation(30, 20));
     }
+    public  static  double sumMultiply(double first, double second) {
+        return sum(first, second) + multiply(first, second);
+    }
+
+    public  static  double sumMinusDivision(double first, double second) {
+        return  minus(first, second) + division(first, second);
+    }
+
+    public  static  double sumAllOperation(double first, double second) {
+        return  sum(first, second) + multiply(first, second) + minus(first, second) + division(first, second);
+    }
+
 }
