21519d285311b49e9519065bab11ecc8492af453
Данил Бодров <Bodrow19999@mail.ru>
2021-10-04 14:38:10 +0400

    0. Что такое тестирование [#159431 #211892]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,10 +2,6 @@ package ru.job4j.converter;
 
 public class Converter {
     public static void main(String[] args) {
-        /*float euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        float dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " dollar.");*/
 
         float in = 140;
         float expected = 2;
