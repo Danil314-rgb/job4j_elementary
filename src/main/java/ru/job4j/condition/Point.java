662eb425453cef44e44b83e43f91ac2150944ebd
Данил Бодров <Bodrow19999@mail.ru>
2021-10-04 16:14:39 +0400

    3. Тесты для определения расстояния между точками. [#159459 #211978]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,16 +2,12 @@ package ru.job4j.condition;
 
 public class Point {
     public static void main(String[] args) {
-
         double result1 = Point.distance(0, 4, 2, 5);
         System.out.println("result2 (0, 4) to (2, 5): " + result1);
-
-        double result2 = Point.distance(0,2,0,1);
+        double result2 = Point.distance(0, 2, 0, 1);
         System.out.println("result2 (0, 2) to (0, 1): " + result2);
-
         double result3 = Point.distance(1, 0, 2, 0);
         System.out.println("result2 (1, 0) to (2, 0): " + result3);
-
         double result4 = Point.distance(1, 2, 3, 4);
         System.out.println("result2 (1, 2) to (3, 4): " + result4);
     }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -2,56 +2,51 @@ package ru.job4j.condition;
 
 import org.junit.Assert;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class PointTest {
 
     @Test
-    public void when04to25then2_3() {
+    public void when04to25Then2() {
         int x1 = 0;
         int y1 = 4;
         int x2 = 2;
         int y2 = 5;
-
         double result = 2.3;
         double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(result, out, 0.1 );
+        Assert.assertEquals(result, out, 0.1);
     }
 
     @Test
-    public void when02to01then1() {
+    public void when02to01Then1() {
         int x1 = 0;
         int y1 = 2;
         int x2 = 0;
         int y2 = 1;
-
         double result = 1;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(result, out, 0.1);
     }
 
     @Test
-    public void when10to20then1() {
+    public void when10to20Then1() {
         int x1 = 1;
         int y1 = 0;
         int x2 = 2;
         int y2 = 0;
-
         double result = 1;
         double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(result, out, 0.1 );
+        Assert.assertEquals(result, out, 0.1);
     }
 
     @Test
-    public void when12to34then2_8() {
+    public void when12to34Then2() {
         int x1 = 1;
         int y1 = 2;
         int x2 = 3;
         int y2 = 4;
-
         double result = 2.8;
         double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(result, out, 0.1 );
+        Assert.assertEquals(result, out, 0.1);
     }
 }
