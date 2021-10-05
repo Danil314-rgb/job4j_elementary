4b9df3fb566699ce379e41a19c8072c9521407fa
Данил Бодров <Bodrow19999@mail.ru>
2021-10-05 16:27:54 +0400

    1.2. Входные данные в тесте [#297750 #211940]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 public class X2 {
+
     public static void main(String[] args) {
         System.out.println(calc(10, 0, 0, 2));
         System.out.println(calc(1, 1, 1, 1));
