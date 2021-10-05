a6925607a4b9714be8f7d0502d61519bc725c2bd
Данил Бодров <Bodrow19999@mail.ru>
2021-10-05 19:07:03 +0400

    1.2. Входные данные в тесте [#297750 #211940]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -12,7 +12,7 @@ public class X2Test {
         int c = 0;
         int x = 2;
         int expected = 40;
-        int result = X2.calc(a, b, c, x);
+        int result = X2.calcs(a, b, c, x);
         Assert.assertEquals(expected, result);
     }
 
@@ -23,7 +23,7 @@ public class X2Test {
         int c = 1;
         int x = 1;
         int expected = 3;
-        int result = X2.calc(a, b, c, x);
+        int result = X2.calcs(a, b, c, x);
         Assert.assertEquals(expected, result);
     }
 
@@ -34,7 +34,7 @@ public class X2Test {
         int c = 1;
         int x = 1;
         int expected = 2;
-        int result = X2.calc(a, b, c, x);
+        int result = X2.calcs(a, b, c, x);
         Assert.assertEquals(expected, result);
     }
 
@@ -45,7 +45,7 @@ public class X2Test {
         int c = 0;
         int x = 1;
         int expected = 2;
-        int result = X2.calc(a, b, c, x);
+        int result = X2.calcs(a, b, c, x);
         Assert.assertEquals(expected, result);
     }
 
@@ -56,7 +56,7 @@ public class X2Test {
         int c = 1;
         int x = 0;
         int expected = 1;
-        int result = X2.calc(a, b, c, x);
+        int result = X2.calcs(a, b, c, x);
         Assert.assertEquals(expected, result);
     }
 }
